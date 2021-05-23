96d650f05a9b2b1b539ccf81f9efcd7c16b2d685
khjkhj1209 <20181019@sungshin.ac.kr>
2021-05-24 04:44:30 +0900

    feat:Picture

--- app/src/main/java/com/example/clova/picture/PictureFragment.java
+++ app/src/main/java/com/example/clova/picture/PictureFragment.java
@@ -1,14 +1,36 @@
 package com.example.clova.picture;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.clova.R;
+import com.example.clova.picture.util.CanvasIO;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,50 +39,221 @@ import com.example.clova.R;
  */
 public class PictureFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private DrawCanvas drawCanvas;
+    private FloatingActionButton fbPen;             //펜 모드 버튼
+    private FloatingActionButton fbEraser;          //지우개 모드 버튼
+    private FloatingActionButton fbSave;            //그림 저장 버튼
+    private FloatingActionButton fbEraserAll;            //그림 모두 지우기
+    private ConstraintLayout canvasContainer;       //캔버스 root view
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private final int PERMISSIONS_REQUEST_RESULT = 100;
 
+    public boolean requestPermissionCamera(){
+        int sdkVersion = Build.VERSION.SDK_INT;
+        if(sdkVersion >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                    || ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                    PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)
+                        || ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                } else {
+                    ActivityCompat.requestPermissions(getActivity(),
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
+                            PERMISSIONS_REQUEST_RESULT);
+                }
+            } else {
+            }
+        } else{
+        }
+        return true;
+    }
     public PictureFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PictureFragment.
-     */
     // TODO: Rename and change types and number of parameters
-    public static PictureFragment newInstance(String param1, String param2) {
-        PictureFragment fragment = new PictureFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static PictureFragment newInstance() {
+        PictureFragment pictureFragment = new PictureFragment();
+        return pictureFragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_picture, container, false);
+        View view = inflater.inflate(R.layout.activity_picture, container, false);
+
+        //startActivity(new Intent(getActivity(), PictureActivity.class));
+        canvasContainer = view.findViewById(R.id.lo_canvas);
+        fbPen = view.findViewById(R.id.fb_pen);
+        fbEraser = view.findViewById(R.id.fb_eraser);
+        fbSave = view.findViewById(R.id.fb_save);
+        fbEraserAll = view.findViewById(R.id.fb_eraer_All);
+        drawCanvas = new DrawCanvas(getContext());
+
+        canvasContainer.addView(drawCanvas);
+
+        setOnClickListener();
+
+        return view;
+    }
+    /**
+     * OnClickListener Setting
+     */
+    private void setOnClickListener() {
+        fbPen.setOnClickListener((v) -> {
+            drawCanvas.changeTool(DrawCanvas.MODE_PEN);
+        });
+
+        fbEraser.setOnClickListener((v) -> {
+            drawCanvas.changeTool(DrawCanvas.MODE_ERASER);
+        });
+
+        fbSave.setOnClickListener((v) -> {
+            requestPermissionCamera();
+            drawCanvas.invalidate();
+            requestPermissionCamera();
+            Bitmap saveBitmap = drawCanvas.getCurrentCanvas();
+            Toast.makeText(getContext(),"저장", Toast.LENGTH_SHORT).show();
+            CanvasIO.saveBitmapToJpg(saveBitmap, getContext());
+        });
+
+
+        fbEraserAll.setOnClickListener((v) -> {
+            drawCanvas.init();
+            //.loadDrawImage = CanvasIO.openBitmap(this);
+            drawCanvas.invalidate();
+        });
+    }
+
+    /**
+     * Pen을 표현할 class입니다.
+     */
+    class Pen {
+        public static final int STATE_START = 0;        //펜의 상태(움직임 시작)
+        public static final int STATE_MOVE = 1;         //펜의 상태(움직이는 중)
+        float x, y;                                     //펜의 좌표
+        int moveStatus;                                 //현재 움직임 여부
+        int color;                                      //펜 색
+        int size;                                       //펜 두께
+
+        public Pen(float x, float y, int moveStatus, int color, int size) {
+            this.x = x;
+            this.y = y;
+            this.moveStatus = moveStatus;
+            this.color = color;
+            this.size = size;
+        }
+
+        /**
+         * jhChoi - 201124
+         * 현재 pen의 상태가 움직이는 상태인지 반환합니다.
+         */
+        public boolean isMove() {
+            return moveStatus == STATE_MOVE;
+        }
+    }
+
+    /**
+     * 그림이 그려질 canvas view
+     */
+    class DrawCanvas extends View {
+        public static final int MODE_PEN = 1;                     //모드 (펜)
+        public static final int MODE_ERASER = 0;                  //모드 (지우개)
+        final int PEN_SIZE = 4;                                   //펜 사이즈
+        final int ERASER_SIZE = 40;                               //지우개 사이즈
+
+        ArrayList<Pen> drawCommandList;                           //그리기 경로가 기록된 리스트
+        Paint paint;                                              //펜
+        Bitmap loadDrawImage;                                     //호출된 이전 그림
+        int color;                                                //현재 펜 색상
+        int size;                                                 //현재 펜 크기
+
+        public DrawCanvas(Context context) {
+            super(context);
+            init();
+        }
+
+        public DrawCanvas(Context context, @Nullable AttributeSet attrs) {
+            super(context, attrs);
+            init();
+        }
+
+        public DrawCanvas(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+            super(context, attrs, defStyleAttr);
+            init();
+        }
+
+        /**
+         * 그리기에 필요한 요소를 초기화 합니다.
+         */
+        private void init() {
+            paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            drawCommandList = new ArrayList<>();
+            loadDrawImage = null;
+            color = Color.BLACK;
+            size = PEN_SIZE;
+        }
+
+        /**
+         * jhChoi - 201124
+         * 현재까지 그린 그림을 Bitmap으로 반환합니다.
+         */
+        public Bitmap getCurrentCanvas() {
+            Bitmap bitmap = Bitmap.createBitmap(this.getWidth(), this.getHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bitmap);
+            this.draw(canvas);
+            return bitmap;
+        }
+
+        /**
+         /**
+         * jhChoi - 201124
+         * Tool type을 (펜 or 지우개)로 변경합니다.
+         */
+        private void changeTool(int toolMode) {
+            if (toolMode == MODE_PEN) {
+                this.color = Color.BLACK;
+                size = PEN_SIZE;
+            } else {
+                this.color = Color.WHITE;
+                size = ERASER_SIZE;
+            }
+            paint.setColor(color);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawColor(Color.WHITE);
+
+            if (loadDrawImage != null) {
+                canvas.drawBitmap(loadDrawImage, 0, 0, null);
+            }
+
+            for (int i = 0; i < drawCommandList.size(); i++) {
+               Pen p = drawCommandList.get(i);
+                paint.setColor(p.color);
+                paint.setStrokeWidth(p.size);
+
+                if (p.isMove()) {
+                    Pen prevP = drawCommandList.get(i - 1);
+                    canvas.drawLine(prevP.x, prevP.y, p.x, p.y, paint);
+                }
+            }
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent e) {
+            int action = e.getAction();
+            int state = action == MotionEvent.ACTION_DOWN ? Pen.STATE_START : Pen.STATE_MOVE;
+            drawCommandList.add(new Pen(e.getX(), e.getY(), state, color, size));
+            invalidate();
+            return true;
+        }
     }
 }
